ba2bc9151bb1c9a32c3f53ce9bedc4857e508817
unknown <kunjpatel6363@gmail.com>
2023-10-19 20:07:53 +0530

    landing page created

--- src/app/components/MaxWidthWrapper.tsx
+++ src/app/components/MaxWidthWrapper.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils"
 import { ReactNode } from "react"
 
 const Maxwidthwrapper = ({
@@ -10,7 +11,7 @@ const Maxwidthwrapper = ({
         children: ReactNode
     }) => {
         return(
-            <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
+            <div className={cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-20', className)}>
                 {children}
             </div>
         )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
-import Image from 'next/image'
+
 import Maxwidthwrapper from './components/MaxWidthWrapper'
 
 export default function Home() {
   return (
-    <Maxwidthwrapper className='bg-red-500'><div></div></Maxwidthwrapper>
+    <Maxwidthwrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
+        <div className='mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
+          <p className='text-sm font-semibold text-gray-700'>
+            Efferves is now live!
+          </p>
+        </div>
+    </Maxwidthwrapper>
   )
 }
